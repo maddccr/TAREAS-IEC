341cec47ba23c554104576ba115aaa14f9618c94
sofialerner2 <sofialerner2@gmail.com>
2025-08-31 00:21:22 -0300

    ..

---
+++ Tarea1/p0ej1.R
@@ -0,0 +1,37 @@
+# Práctico 0 - Ejercicio 1
+
+# Parte b)
+dist_normal <- function(mu = 0,sigma = 1,delta){  # Función que genera densidades de una normal
+  x <- seq(-5, 5, by = delta) # valores de x de -5 a 5
+  probs <- (1/(sqrt(2*pi)*sigma)) * exp(-((x - mu)^2)/(2*sigma^2)) 
+  return(list(x = x, probs = probs)) # devuelve lista con x y densidades
+}
+
+# Parte c) Grafica la normal con diferentes distancias entre los puntos
+delta <- c(0.5, 0.1, 0.01)
+for(d in delta){
+  dt <- dist_normal(0,1,d)
+  plot(dt$x, dt$probs, col = "red", xlab = "x", ylab = "Densidad",
+       cex = d, main = "Densidad Distribución Normal típica",
+       sub = paste0("(con distancia entre valores = ", d, ")"))
+}
+
+# Parte d)
+
+par(mfrow = c(1,3)) # Prepara ventana para 3 gráficos juntos
+
+# Diferentes combinaciones de media y desviación
+parametros <- list(
+  c(mu = 0, sigma = 1),
+  c(mu = 5, sigma = 1),
+  c(mu = 5, sigma = 4)
+)
+
+for(p in parametros){
+  mu <- p["mu"]
+  sigma <- p["sigma"]
+  dt <- dist_normal(mu, sigma, delta = 0.01)
+  plot(dt$x, dt$probs, col = "red", xlab = "x", ylab = "Densidad", type = "l",
+       lwd = 2, ylim = c(0, 0.4), main = paste0("Normal (μ=", mu, ",σ=", sigma, ")"))
+}
+
